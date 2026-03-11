ac45db4e7ea4c254dbd49515be22a26f46577f46
haysonyuen <haysonyuenyyh@gmail.com>
2026-03-11 16:13:14 -0700

    Add WF Savings to Hayson's account list

--- src/api.js
+++ src/api.js
@@ -43,7 +43,7 @@ const ADMIN_EMAILS = new Set(
 // Per-user account restrictions: email → array of allowed account aliases (with last4).
 // Admins are unrestricted. Users not listed here see no accounts by default.
 const USER_ACCOUNT_LIMITS = {
-  'haysonyuenyyh@gmail.com': ['Chase Freedom (4844)'],
+  'haysonyuenyyh@gmail.com': ['WF Savings (0954)', 'Chase Freedom (4844)'],
   'favouritemie@gmail.com':  ['Chase Freedom (4844)'],
 };
 
